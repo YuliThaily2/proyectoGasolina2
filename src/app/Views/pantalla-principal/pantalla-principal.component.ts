5bd55b9d5616bc1f8d4dd3ce464de4877b948f83
yuli <yulissathaily123@gmail.com>
2024-04-27 17:53:54 -0600

    PantallaPrincipal

---
+++ src/app/Views/pantalla-principal/pantalla-principal.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla-principal',
+  templateUrl: './pantalla-principal.component.html',
+  styleUrl: './pantalla-principal.component.css'
+})
+
+export class PantallaPrincipalComponent {
+  arrFecha = [
+    '2024',
+    '2023',
+    '2022',
+    '2021',
+    '2020',
+    '2019',
+    '2018',
+    '2017',
+    '2016',
+  ];
+
+  arrGasolina = [
+    'Gasolina Magna',
+    'Gasolina Premium',  
+  ];
+
+  estadosMexico = [
+    'Aguascalientes',
+    'Baja California',
+    'Baja California Sur',
+    'Campeche',
+    'Chiapas',
+    'Chihuahua',
+    'Ciudad de México',
+    'Coahuila',
+    'Colima',
+    'Durango',
+    'Guanajuato',
+    'Guerrero',
+    'Hidalgo',
+    'Jalisco',
+    'México',
+    'Michoacán',
+    'Morelos',
+    'Nayarit',
+    'Nuevo León',
+    'Oaxaca',
+    'Puebla',
+    'Querétaro',
+    'Quintana Roo',
+    'San Luis Potosí',
+    'Sinaloa',
+    'Sonora',
+    'Tabasco',
+    'Tamaulipas',
+    'Tlaxcala',
+    'Veracruz',
+    'Yucatán',
+    'Zacatecas'
+  ];
+  
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PantallaPrincipalComponent } from './Views/pantalla-principal/pantalla-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'PantallaPrincipal', component:PantallaPrincipalComponent},
 
+
+  {path:'',pathMatch:'full',redirectTo:'PantallaPrincipal'},
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
