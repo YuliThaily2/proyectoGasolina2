2b681d20fd53baaea394b5c54aea58dce0f69894
Desisse <20151707@aguascalientes.tecnm.mx>
2024-04-28 00:33:36 -0600

    Estilos pantalla principal y realizacion de la bienvenida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PantallaPrincipalComponent } from './Views/pantalla-principal/pantalla-principal.component';
+import { BienvenidaComponent } from './Views/bienvenida/bienvenida.component';
 
 const routes: Routes = [
+  {path: 'Bienvenida', component:BienvenidaComponent},
   {path: 'PantallaPrincipal', component:PantallaPrincipalComponent},
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PantallaPrincipalComponent } from './Views/pantalla-principal/pantalla-principal.component';
 import { FormsModule } from '@angular/forms';
+import { BienvenidaComponent } from './Views/bienvenida/bienvenida.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PantallaPrincipalComponent
+    PantallaPrincipalComponent,
+    BienvenidaComponent
   ],
   imports: [
     BrowserModule,
