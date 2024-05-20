dabd8191d27ed4d01be79d5dc4f4b9a166bedc66
Desisse <20151707@aguascalientes.tecnm.mx>
2024-05-20 08:32:00 -0600

    Conexion hacia el front y realiza la prediccion

--- src/app/Views/pantalla-prediccion/pantalla-prediccion.component.ts
+++ src/app/Views/pantalla-prediccion/pantalla-prediccion.component.ts
@@ -1,30 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-pantalla-prediccion',
   templateUrl: './pantalla-prediccion.component.html',
   styleUrl: './pantalla-prediccion.component.css'
 })
-export class PantallaPrediccionComponent {
+export class PantallaPrediccionComponent implements OnInit{
   year: string;
+  month: string;
   state: string;
   gasolina: string;
+  prediccion: number | null;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.year = '';
+    this.month = '';
     this.state = '';
     this.gasolina = '';
+    this.prediccion = null;
   }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const yearParam = params.get('year');
+      const monthParam = params.get('month');
       const stateParam = params.get('state');
       const gasolinaParam = params.get('gasolina');
 
       if (yearParam !== null) {
         this.year = yearParam;
+      }
+      if (monthParam !== null) {
+        this.month = monthParam;
       }
       if (stateParam !== null) {
         this.state = stateParam;
@@ -32,6 +42,26 @@ export class PantallaPrediccionComponent {
       if (gasolinaParam !== null) {
         this.gasolina = gasolinaParam;
       }
+    this.getPrediction;
     });
   }
+
+  async getPrediction() {
+    try {
+      const response = await firstValueFrom(this.http.post<{ Predicción: number }>('http://localhost:5000/prediccion', {
+        Ciudad: this.state, // Cambia 'Estado' por 'Ciudad' según el orden especificado
+        TipoGasolina: this.gasolina, // Agrega el tipo de gasolina antes del año y mes
+        Año: parseInt(this.year, 10),
+        Mes: parseInt(this.month, 10) // Envía el número del mes como un número entero
+      }));
+      this.prediccion = response.Predicción;
+  
+      // Imprimir la predicción en la consola
+      console.log('Predicción obtenida:', this.prediccion);
+    } catch (error) {
+      console.error('Error fetching prediction:', error);
+    }
+  }
+    
+  
 }
--- src/app/Views/pantalla-principal/pantalla-principal.component.ts
+++ src/app/Views/pantalla-principal/pantalla-principal.component.ts
@@ -9,13 +9,15 @@ import { Router } from '@angular/router';
 
 export class PantallaPrincipalComponent {
   selectedYear: string;
+  selectedMes: string;
   selectedState: string;
   selectedGasolina: string;
 
   constructor(private router: Router){
-    this.selectedYear= '10';
-    this.selectedState = 'As';
-    this.selectedGasolina = 'Ma';
+    this.selectedYear= '';
+    this.selectedMes = '';
+    this.selectedState = '';
+    this.selectedGasolina = '';
 
   }
 
@@ -25,6 +27,26 @@ export class PantallaPrincipalComponent {
     this.selectedState = this.estadosMexico[0];
     this.selectedGasolina = this.arrGasolina[0];
   }
+
+  // meses: string[] = [
+  //   'Enero',
+  //   'Febrero',
+  //   'Marzo',
+  //   'Abril',
+  //   'Mayo',
+  //   'Junio',
+  //   'Julio',
+  //   'Agosto',
+  //   'Septiembre',
+  //   'Octubre',
+  //   'Noviembre',
+  //   'Diciembre'
+  // ];
+
+  meses: number[] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+  ];
+  
 
   arrFecha = [
     '2030',
@@ -45,48 +67,50 @@ export class PantallaPrincipalComponent {
   ];
 
   arrGasolina = [
-    'Gasolina Magna',
-    'Gasolina Premium',  
+    'Gasolina de alto octanaje', 
+    'Gasolina de bajo octanaje'  
   ];
 
   estadosMexico = [
-    'Aguascalientes',
-    'Baja California',
-    'Baja California Sur',
-    'Campeche',
-    'Chiapas',
-    'Chihuahua',
-    'Ciudad de México',
-    'Coahuila',
-    'Colima',
-    'Durango',
-    'Guanajuato',
-    'Guerrero',
-    'Hidalgo',
-    'Jalisco',
-    'México',
-    'Michoacán',
-    'Morelos',
-    'Nayarit',
-    'Nuevo León',
-    'Oaxaca',
-    'Puebla',
-    'Querétaro',
-    'Quintana Roo',
-    'San Luis Potosí',
-    'Sinaloa',
-    'Sonora',
-    'Tabasco',
-    'Tamaulipas',
-    'Tlaxcala',
-    'Veracruz',
-    'Yucatán',
-    'Zacatecas'
+    'Acapulco, Gro.','Aguascalientes, Ags.','Atlacomulco, Edo. de Méx.','Campeche, Camp.',
+            'Cancún, Q. Roo.','Cd. Acuña, Coah.','Cd. Juárez, Chih.','Chetumal, Q. Roo.','Chihuahua, Chih.','Coatzacoalcos, Ver.','Colima, Col.','Cortazar, Gto.',
+            'Cuernavaca, Mor.','Culiacán, Sin.','Córdoba, Ver.','Durango, Dgo.','Esperanza, Son.','Fresnillo, Zac.',
+            'Guadalajara, Jal.','Hermosillo, Son.','Huatabampo, Son.','Iguala, Gro.','Izúcar de Matamoros, Pue.',
+            'Jacona, Mich.','Jiménez, Chih.','La Paz, B.C.S.',
+'León, Gto.',
+'Matamoros, Tamps.',
+'Mexicali, B.C.',
+'Monclova, Coah.',
+'Monterrey, N.L.',
+'Morelia, Mich.',
+'Mérida, Yuc.',
+'Oaxaca, Oax.',
+'Pachuca, Hgo.',
+'Puebla, Pue.',
+'Querétaro, Qro.',
+'Saltillo, Coah.',
+'San Andrés Tuxtla, Ver.',
+'San Luis Potosí, S.L.P.',
+'Tampico, Tamps.',
+'Tapachula, Chis.',
+'Tehuantepec, Oax.',
+'Tepatitlán, Jal.',
+'Tepic, Nay.',
+'Tijuana, B.C.',
+'Tlaxcala, Tlax.',
+'Toluca, Edo. de Méx.',
+'Torreón, Coah.',
+'Tulancingo, Hgo.',
+'Tuxtla Gutiérrez, Chis.',
+'Veracruz, Ver.',
+'Villahermosa, Tab.',
+'Zacatecas, Zac.',
+'Área Met. de la Cd. de México'
   ];
 
   onSubmit() {
-    this.router.navigate(['/PantallaPrediccion', { year: this.selectedYear, state: this.selectedState, gasolina: this.selectedGasolina }]);
-    console.log(this.selectedYear)
+    this.router.navigate(['/PantallaPrediccion', { year: this.selectedYear, month: this.selectedMes, state: this.selectedState, gasolina: this.selectedGasolina }]);    console.log(this.selectedYear)
+    console.log(this.selectedMes)
     console.log(this.selectedState)
     console.log(this.selectedGasolina)
 
